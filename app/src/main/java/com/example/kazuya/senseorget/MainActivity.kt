159773d0b24e3b47664cd9055b46ddd8a9d82b6f
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-07-06 23:16:51 +0900

    センサー情報表示

--- app/src/main/java/com/example/kazuya/senseorget/MainActivity.kt
+++ app/src/main/java/com/example/kazuya/senseorget/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.kazuya.senseorget
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val buffer = StringBuilder()
+
+        val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        val sList:List<Sensor> = sensorManager.getSensorList(Sensor.TYPE_ALL)
+        for (sensor:Sensor in sList){
+            buffer.append("${sensor.type},${sensor.name},${sensor.vendor}/n")
+        }
+        txt01.text = buffer.toString()
     }
 }
